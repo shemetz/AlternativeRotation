f8ce08a9256ac6e61086939f6435c3e53ad19d94
itamar <itamarcu@gmail.com>
2022-01-08 10:38:44 +0200

    Refactored & fixed;  now uses keybindings, default R with Shift modifier, and has better UI

--- scripts/alternative-rotation.js
+++ scripts/alternative-rotation.js
@@ -1,27 +1,32 @@
 const MODULE_ID = 'alternative-rotation'
-const radToDeg = 180 / Math.PI
-const degToRad = Math.PI / 180
+const TAU = Math.PI * 2
+const radToDeg = 360 / TAU
+const degToRad = TAU / 360
+
 let visualEffectsGraphics = null
-let currentMim = null
-let isNowRotating = false
-let isAvoidingRefresh = false
-let isRotatingMultipleTokens = false
+let currentlyRotatedObjects = []
+
+let isRotationButtonHeld = false
+let isSnapButtonHeld = false
+
+function isNowRotating () {
+  return currentlyRotatedObjects.length > 0
+}
+
+function isNowRotatingMultiple () {
+  return currentlyRotatedObjects.length > 1
+}
+
+function isSnapRotationButtonHeld () {
+  return isSnapButtonHeld
+}
 
 function getSetting (settingName) {
   return game.settings.get(MODULE_ID, settingName)
 }
 
-function isDragButtonHeld () {
-  return isNewerVersion(game.version, '9.231') ? game.keyboard.downKeys.has('SHIFT') : game.keyboard._downKeys.has('Shift')
-}
-
-function isDragSnapButtonHeld () {
-  return isNewerVersion(game.version, '9.231') ? game.keyboard.downKeys.has('CONTROL') : game.keyboard._downKeys.has('Control')
-}
-
-function isDoingDrag (mouseInteractionManager) {
-  const obj = mouseInteractionManager.object
-  return !mouseInteractionManager._dragRight && (obj instanceof Token || obj instanceof Tile)
+function getMousePosition () {
+  return canvas.app.renderer.plugins.interaction.mouse.getLocalPosition(canvas.app.stage)
 }
 
 function getVisualEffectsGraphics () {
@@ -48,10 +53,14 @@ function controlledObjectsOnCurrentLayer () {
   return []
 }
 
-function drawDirectionalArrow (from, to) {
-  const width = isDragSnapButtonHeld() ? 8 : 5
+function drawDirectionalArrow () {
+  const object = currentlyRotatedObjects[0]
+  const from = getCenter(object)
+  const to = getMousePosition()
+  const width = 5
   const color = 0xFF9829
   const alpha = 0.8
+  const alphaMainArrow = isSnapRotationButtonHeld() ? 0.3 : 0.8
   const circleRadius = 10
   const arrowCornerLength = 30
   const arrowCornerAngle = 150 * degToRad
@@ -68,22 +77,42 @@ function drawDirectionalArrow (from, to) {
     x: to.x + Math.cos(angle - arrowCornerAngle) * arrowCornerLength,
     y: to.y + Math.sin(angle - arrowCornerAngle) * arrowCornerLength,
   }
-  getVisualEffectsGraphics().clear()
-      .lineStyle(width, color, alpha) // width, color, alpha
-      .drawCircle(from.x, from.y, circleRadius)
-      .drawPolygon(arrowStart.x, arrowStart.y, to.x, to.y)
-      .drawPolygon(to.x, to.y, arrowCorner1.x, arrowCorner1.y, to.x, to.y, arrowCorner2.x, arrowCorner2.y)
+  getVisualEffectsGraphics().clear().lineStyle(width, color, alpha).drawCircle(from.x, from.y, circleRadius).lineStyle(width, color, alphaMainArrow).drawPolygon(arrowStart.x, arrowStart.y, to.x, to.y).drawPolygon(to.x, to.y, arrowCorner1.x, arrowCorner1.y, to.x, to.y, arrowCorner2.x, arrowCorner2.y)
+
+  if (isSnapRotationButtonHeld()) {
+    const snappedRotation = rotationTowardsCursor(object, to) * degToRad + TAU / 4
+    const secondaryArrowLength = 200
+    const to2 = {
+      x: from.x + Math.cos(snappedRotation) * secondaryArrowLength,
+      y: from.y + Math.sin(snappedRotation) * secondaryArrowLength
+    }
+    const secondArrowStart = {
+      x: from.x + Math.cos(snappedRotation) * (circleRadius - width / 2 - 2),
+      y: from.y + Math.sin(snappedRotation) * (circleRadius - width / 2 - 2),
+    }
+    const secondArrowCorner1 = {
+      x: to2.x + Math.cos(snappedRotation + arrowCornerAngle) * arrowCornerLength,
+      y: to2.y + Math.sin(snappedRotation + arrowCornerAngle) * arrowCornerLength,
+    }
+    const secondArrowCorner2 = {
+      x: to2.x + Math.cos(snappedRotation - arrowCornerAngle) * arrowCornerLength,
+      y: to2.y + Math.sin(snappedRotation - arrowCornerAngle) * arrowCornerLength,
+    }
+    getVisualEffectsGraphics().lineStyle(5, color, alpha).drawPolygon(secondArrowStart.x, secondArrowStart.y, to2.x, to2.y).drawPolygon(to2.x, to2.y, secondArrowCorner1.x, secondArrowCorner1.y, to2.x, to2.y, secondArrowCorner2.x, secondArrowCorner2.y)
+  }
 }
 
-function drawMultiRotationVFX (focusPoint) {
-  const width = isDragSnapButtonHeld() ? 6 : 4
+function drawMultiRotationVFX () {
+  const focusPoint = getMousePosition()
+  const width = 4
   const color = 0xFF9829
-  const circleAlpha = 0.5
+  const circleAlpha = isSnapRotationButtonHeld() ? 0.2 : 0.5
+  const alphaMainArrows = isSnapRotationButtonHeld() ? 0.2 : 0.8
+  const otherArrowsAlpha = 0.8
   const circleRadius = 14
   // draw circle
-  getVisualEffectsGraphics().clear()
-      .lineStyle(width, color, circleAlpha) // width, color, alpha
-      .drawCircle(focusPoint.x, focusPoint.y, circleRadius)
+  getVisualEffectsGraphics().clear().lineStyle(width, color, circleAlpha)
+    .drawCircle(focusPoint.x, focusPoint.y, circleRadius)
   const arrowLength = 18
   const arrowCornerLength = 12
   const arrowCornerAngle = 30 * degToRad
@@ -110,11 +139,29 @@ function drawMultiRotationVFX (focusPoint) {
       x: to.x + Math.cos(angle - arrowCornerAngle) * arrowCornerLength,
       y: to.y + Math.sin(angle - arrowCornerAngle) * arrowCornerLength,
     }
-    const alpha = 0.8
-    getVisualEffectsGraphics()
-        .lineStyle(width, color, alpha) // width, color, alpha
-        .drawPolygon(arrowStart.x, arrowStart.y, to.x, to.y)
-        .drawPolygon(to.x, to.y, arrowCorner1.x, arrowCorner1.y, to.x, to.y, arrowCorner2.x, arrowCorner2.y)
+    getVisualEffectsGraphics().lineStyle(width, color, alphaMainArrows)
+      .drawPolygon(arrowStart.x, arrowStart.y, to.x, to.y).drawPolygon(to.x, to.y, arrowCorner1.x, arrowCorner1.y, to.x, to.y, arrowCorner2.x, arrowCorner2.y)
+
+    // secondary arrows from each target
+    const snappedRotation = rotationTowardsCursor(object, to) * degToRad + TAU / 4
+    const secondaryArrowLength = 100
+    const to2 = {
+      x: objCenter.x + Math.cos(snappedRotation) * secondaryArrowLength,
+      y: objCenter.y + Math.sin(snappedRotation) * secondaryArrowLength
+    }
+    const secondArrowStart = {
+      x: objCenter.x + Math.cos(snappedRotation) * (circleRadius - width / 2 - 2),
+      y: objCenter.y + Math.sin(snappedRotation) * (circleRadius - width / 2 - 2),
+    }
+    const secondArrowCorner1 = {
+      x: to2.x + Math.cos(snappedRotation + arrowCornerAngle + TAU / 2) * arrowCornerLength,
+      y: to2.y + Math.sin(snappedRotation + arrowCornerAngle + TAU / 2) * arrowCornerLength,
+    }
+    const secondArrowCorner2 = {
+      x: to2.x + Math.cos(snappedRotation - arrowCornerAngle + TAU / 2) * arrowCornerLength,
+      y: to2.y + Math.sin(snappedRotation - arrowCornerAngle + TAU / 2) * arrowCornerLength,
+    }
+    getVisualEffectsGraphics().lineStyle(5, color, otherArrowsAlpha).drawPolygon(secondArrowStart.x, secondArrowStart.y, to2.x, to2.y).drawPolygon(to2.x, to2.y, secondArrowCorner1.x, secondArrowCorner1.y, to2.x, to2.y, secondArrowCorner2.x, secondArrowCorner2.y)
   })
 }
 
@@ -132,75 +179,23 @@ function getCenter (object) {
  */
 function rotationTowardsCursor (object, cursor) {
   const obj = getCenter(object)
-  const target = Math.atan2(cursor.y - obj.y, cursor.x - obj.x) + Math.PI * 3 / 2 // down = 0
+  const target = Math.atan2(cursor.y - obj.y, cursor.x - obj.x) + TAU * 3 / 4 // down = 0
   const degrees = target * radToDeg
   const dBig = canvas.grid.type > CONST.GRID_TYPES.SQUARE ? 60 : 45
   const dSmall = getSetting('smooth-rotation') ? 0.1 : 5
-  const snap = isDragSnapButtonHeld() ? dBig : dSmall
+  const snap = isSnapRotationButtonHeld() ? dBig : dSmall
   return Math.round(degrees / snap) * snap
 }
 
-function _handleDragStart_Override (_handleDragStart, event) {
-  // Wrap unless shift+leftpress on a tile or token
-  if (!isDoingDrag(this) || !isDragButtonHeld(event)) {
-    if (isDragButtonHeld(event) && controlledObjectsOnCurrentLayer().length >= 2) {
-      isRotatingMultipleTokens = true
-      drawMultiRotationVFX(event.data.destination)
-      return
-    }
+function _onMouseMove_Override (_onMouseMove, event) {
+  if (!isNowRotating()) {
     // Call wrapped function
-    return _handleDragStart.bind(this)(event)
+    return _onMouseMove.bind(this)(event)
   }
-  // Start drag rotation
-  isNowRotating = true
-  currentMim = this
-}
-
-function _handleMouseOut_Override (_handleMouseOut, event) {
-  if (!event.currentTarget) {
-    return _handleMouseOut.bind(this)(event)
-  }
-  // special case: when user shift-drags from a deselected token
-  // (here `this` is the canvas, so we need to get the token/tile's mouse interaction manager)
-  const mim = event.currentTarget.mouseInteractionManager
-  const obj = mim ? mim.object : null
-  // lots of checks because handleMouseOut can be called by anyone at any point
-  if (
-      mim && isDoingDrag(mim) && !isNowRotating
-      && isDragButtonHeld(event) && event.data.originalEvent.buttons === 1
-      && game.activeTool === 'select' && this.state === this.states.CLICKED
-      && (game.user.isGM || (!game.paused && (obj.actor && obj.actor.hasPerm(game.user, 'OWNER'))))
-  ) {
-    // Start drag rotation
-    isNowRotating = true
-    mim.state = this.states.DRAG
-    // activating drag events so that _handleDragMove_Override will be called from now on
-    mim._activateDragEvents()
-    currentMim = mim
-  }
-  // calling wrapper function either way
-  return _handleMouseOut.bind(this)(event)
-}
-
-function _onDragLeftStart_Override (_onDragLeftStart, event) {
-  const oe = event.data.originalEvent
-  if (
-      (oe.ctrlKey || oe.metaKey) && oe.shiftKey && this.activeLayer.name === 'TokenLayer'
-  ) {
-    // do nothing; preventing the ctrl+drag ruler shortcut
-    // but also, to prevent a weird bug, remove preview:
-    if ( this.preview ) this.preview.removeChildren();
-    event.data.preview = null;
-    return
-  }
-  return _onDragLeftStart.bind(this)(event)
-}
-
-function _handleDragMove_Override (_handleDragMove, event) {
-  if (isRotatingMultipleTokens) {
-    drawMultiRotationVFX(event.data.destination)
+  if (isNowRotatingMultiple()) {
+    drawMultiRotationVFX(getMousePosition())
     const updates = controlledObjectsOnCurrentLayer().map(object => {
-      const angle = rotationTowardsCursor(object, event.data.destination)
+      const angle = rotationTowardsCursor(object, getMousePosition())
       if (getSetting('fast-preview')) {
         // fast preview:  rotate image of token/tile in client, which feels very fast
         if (object instanceof Token)
@@ -218,120 +213,79 @@ function _handleDragMove_Override (_handleDragMove, event) {
       const documentName = controlledObjectsOnCurrentLayer()[0].document.documentName
       canvas.scene.updateEmbeddedDocuments(documentName, updates)
     }
-    return
-  }
-  if (!isDoingDrag(this) || !isNowRotating) {
     // Call wrapped function
-    return _handleDragMove.bind(this)(event)
-  }
-
-  // Continue drag rotation, showing "preview"
-  const object = this.object
-  const cursor = event.data.destination
-  const targetRotation = rotationTowardsCursor(object, cursor)
-  // draw arrow
-  const start = getCenter(object)
-  drawDirectionalArrow(start, cursor)
-  if (getSetting('fast-preview')) {
-    // fast preview:  rotate image of token/tile in client, which feels very fast
-    if (object instanceof Token)
-      object.icon.rotation = targetRotation * degToRad
-    else
-      object.tile.img.rotation = targetRotation * degToRad
+    return _onMouseMove.bind(this)(event)
   } else {
-    // not fast preview:  rotate data of token/tile.  will be sent to remote server (and other players), but lag
-    object.document.update({ rotation: targetRotation })
-  }
-}
-
-function completeDragRotation (mim, event) {
-  const object = mim.object
-  const targetRotation = rotationTowardsCursor(object, event.data.destination)
-  object.rotate(targetRotation)
-  mim.state = mim.states.DROP
-}
-
-function _handleDragDrop_Override (_handleDragDrop, event) {
-  if (!isDoingDrag(this) || !isNowRotating) {
-    if (isRotatingMultipleTokens) {
-      completeMultiRotation(this, event)
-      return
+    const object = currentlyRotatedObjects[0]
+    // draw arrow
+    drawDirectionalArrow()
+    // Continue rotation
+    const cursor = getMousePosition()
+    const targetRotation = rotationTowardsCursor(object, cursor)
+    if (getSetting('fast-preview')) {
+      // fast preview:  rotate image of token/tile in client, which feels very fast
+      if (object instanceof Token)
+        object.icon.rotation = targetRotation * degToRad
+      else
+        object.tile.img.rotation = targetRotation * degToRad
+    } else {
+      // not fast preview:  rotate data of token/tile.  will be sent to remote server (and other players), but lag
+      object.document.update({ rotation: targetRotation })
     }
     // Call wrapped function
-    return _handleDragDrop.bind(this)(event)
+    return _onMouseMove.bind(this)(event)
   }
-  // Complete drag rotation
-  completeDragRotation(this, event)
 }
 
-function _handleMouseUp_Override (_handleMouseUp, event) {
-  // workaround to solve an edge case bug that should drop the drag when mouse is let go:
-  // when letting go of the mouse while hovering over a second token with
-  if (isNowRotating && this.state === this.states.HOVER) {
-    return completeDragRotation(currentMim, event)
-  }
-  return _handleMouseUp.bind(this)(event)
-}
-
-function _handleDragCancel_Override (_handleDragCancel, event) {
-  if (!isDoingDrag(this) || !isNowRotating) {
-    // Call wrapped function
-    return _handleDragCancel.bind(this)(event)
-  }
-  isNowRotating = false
-  if (this.state === this.states.DRAG) {
-    const object = this.object
-    // Cancel drag rotation
-    // reset rotation to match data
-    if (object instanceof Token)
-      object.icon.rotation = object.data.rotation
-    else
-      object.tile.img.rotation = object.data.rotation
-  }
+function completeRotation () {
   getVisualEffectsGraphics().clear()
-  isAvoidingRefresh = true
-  this.object.control()
-  isAvoidingRefresh = false
-  this.state = this.states.NONE
-  currentMim = null
-}
-
-function _onControl_Override (_onControl, { releaseOthers = true, updateSight = true, pan = false } = {}) {
-  if (!isAvoidingRefresh)
-    return _onControl.bind(this)({ releaseOthers, updateSight, pan })
-  this.zIndex = 1
-  //NOTABLY ABSENT:
-  // this.refresh();
-  if (pan) canvas.addPendingOperation('Canvas.animatePan', canvas.animatePan, canvas, [{ x: this.x, y: this.y }])
-  canvas.perception.schedule({
-    sight: { initialize: true, refresh: true },
-    lighting: { refresh: true },
-    sounds: { refresh: true },
-    foreground: { refresh: true },
-  })
-}
-
-function _onClickStart_Override (_onClickStart, event) {
-  if (isDragButtonHeld(event) && controlledObjectsOnCurrentLayer().length >= 2) {
-    // do nothing; preventing the release of all controlled tokens
-    return
-  }
-  return _onClickStart.bind(this)(event)
-}
-
-function completeMultiRotation (mim, event) {
-  isRotatingMultipleTokens = false
-  getVisualEffectsGraphics().clear()
-  const updates = controlledObjectsOnCurrentLayer().map(object => {
+  const updates = currentlyRotatedObjects.map(object => {
     let update = { _id: object.id }
-    const angle = rotationTowardsCursor(object, event.data.destination)
+    const angle = rotationTowardsCursor(object, getMousePosition())
     const rotation = object._updateRotation({ angle })
     mergeObject(update, { rotation })
     return update
   })
   if (updates.length > 0) {
-    const documentName = controlledObjectsOnCurrentLayer()[0].document.documentName
+    const documentName = currentlyRotatedObjects[0].document.documentName
     canvas.scene.updateEmbeddedDocuments(documentName, updates)
+  }
+  currentlyRotatedObjects = []
+}
+
+const onRotateButtonDown = () => {
+  isRotationButtonHeld = true
+  const controlled = controlledObjectsOnCurrentLayer()
+  currentlyRotatedObjects = controlled
+  if (controlled.length >= 2) {
+    drawMultiRotationVFX(getMousePosition())
+  } else if (controlled.length === 1) {
+    drawDirectionalArrow()
+  }
+}
+
+const onRotateButtonUp = () => {
+  isRotationButtonHeld = false
+  if (isNowRotating()) {
+    completeRotation()
+  }
+}
+
+const onSnapButtonDown = () => {
+  isSnapButtonHeld = true
+  if (currentlyRotatedObjects.length >= 2) {
+    drawMultiRotationVFX(getMousePosition())
+  } else if (currentlyRotatedObjects.length === 1) {
+    drawDirectionalArrow()
+  }
+}
+
+const onSnapButtonUp = () => {
+  isSnapButtonHeld = false
+  if (currentlyRotatedObjects.length >= 2) {
+    drawMultiRotationVFX(getMousePosition())
+  } else if (currentlyRotatedObjects.length === 1) {
+    drawDirectionalArrow()
   }
 }
 
@@ -360,27 +314,36 @@ Hooks.once('setup', function () {
     return
   }
 
-  libWrapper.register(MODULE_ID,
-      'MouseInteractionManager.prototype._handleDragStart', _handleDragStart_Override, 'MIXED')
-  libWrapper.register(MODULE_ID,
-      'MouseInteractionManager.prototype._handleDragMove', _handleDragMove_Override, 'MIXED')
-  libWrapper.register(MODULE_ID,
-      'MouseInteractionManager.prototype._handleDragDrop', _handleDragDrop_Override, 'MIXED')
-  libWrapper.register(MODULE_ID,
-      'MouseInteractionManager.prototype._handleDragCancel', _handleDragCancel_Override, 'MIXED')
-  libWrapper.register(MODULE_ID,
-      'MouseInteractionManager.prototype._handleMouseOut', _handleMouseOut_Override, 'MIXED')
-  libWrapper.register(MODULE_ID,
-      'MouseInteractionManager.prototype._handleMouseUp', _handleMouseUp_Override, 'MIXED')
-  libWrapper.register(MODULE_ID,
-      'Token.prototype._onControl', _onControl_Override, 'MIXED')
-  libWrapper.register(MODULE_ID,
-      'Canvas.prototype._onDragLeftStart', _onDragLeftStart_Override, 'MIXED')
-  libWrapper.register(MODULE_ID,
-      'PlaceablesLayer.prototype._onClickLeft', _onClickStart_Override, 'MIXED')
+  libWrapper.register(MODULE_ID, 'Canvas.prototype._onMouseMove', _onMouseMove_Override, 'MIXED')
 
-  // TODO:  update to allow keybindings (e.g. R) instead of always Shift and Ctrl.
-    
+  const { SHIFT, ALT, CONTROL } = KeyboardManager.MODIFIER_KEYS
+  game.keybindings.register(MODULE_ID, 'alternative-rotation', {
+    name: 'Alternative Rotation',
+    hint: 'Hold this key while having token(s) selected to make the token(s) turn towards the cursor.' +
+      ' Hold Shift to snap to grid directions (45°/60°).',
+    editable: [
+      {
+        key: 'KeyR',
+      },
+    ],
+    reservedModifiers: [SHIFT, ALT, CONTROL],
+    onDown: () => { onRotateButtonDown() },
+    onUp: () => { {onRotateButtonUp()} },
+  })
+  game.keybindings.register(MODULE_ID, 'alternative-rotation-snap', {
+    name: 'Alternative Rotation (snap)',
+    hint: 'Hold this modifier key while using Alternative Rotation to snap to grid directions (45°/60°).',
+    editable: [
+      {
+        key: ['ShiftLeft']
+      },
+      {
+        key: ['ShiftRight']
+      },
+    ],
+    onDown: () => { onSnapButtonDown() },
+    onUp: () => { { onSnapButtonUp() } },
+  })
   console.log(`Alternative Rotation | initialized`)
 })
 
